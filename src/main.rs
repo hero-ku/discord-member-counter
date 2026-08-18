bf4ade467a39a42c87becba4ff3f10dd7243fc70
Andrew Lynch <a_t_lynch@protonmail.ch>
2026-08-17 21:10:38 -0500

    Update active counters based on member joins, leaves, and updates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use std::sync::Mutex;
+
 use poise::serenity_prelude as serenity;
 
+use crate::counter::MemberCounter;
+
+pub mod counter;
+
+struct Data {
+    counters: Mutex<Vec<MemberCounter>>,
+}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 
 #[tokio::main]
@@ -9,12 +18,21 @@ async fn main() {
         .union(serenity::GatewayIntents::GUILD_MEMBERS)
         .difference(serenity::GatewayIntents::GUILD_SCHEDULED_EVENTS);
 
+    let counters = Vec::new();
+
     let framework = poise::Framework::builder()
-        .options(poise::FrameworkOptions::<(), Error> {
+        .options(poise::FrameworkOptions {
             event_handler: |ctx, event, framework, data| {
                 Box::pin(event_handler(ctx, event, framework, data))
             },
             ..Default::default()
+        })
+        .setup(move |_ctx, _ready, _framework| {
+            Box::pin(async move {
+                Ok(Data {
+                    counters: Mutex::new(counters),
+                })
+            })
         })
         .build();
 
@@ -28,12 +46,43 @@ async fn main() {
 async fn event_handler(
     ctx: &serenity::Context,
     event: &serenity::FullEvent,
-    _framework: poise::FrameworkContext<'_, (), Error>,
-    _data: &(),
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+    data: &Data,
 ) -> Result<(), Error> {
     match event {
         serenity::FullEvent::GuildMemberAddition { new_member } => {
-            todo!()
+            let mut counters = data.counters.lock().unwrap();
+
+            for counter in counters.iter_mut() {
+                counter.member_joined(new_member);
+            }
+        }
+        serenity::FullEvent::GuildMemberRemoval {
+            member_data_if_available,
+            ..
+        } => {
+            if let Some(old_member) = member_data_if_available {
+                let mut counters = data.counters.lock().unwrap();
+
+                for counter in counters.iter_mut() {
+                    counter.member_left(old_member);
+                }
+            }
+        }
+        serenity::FullEvent::GuildMemberUpdate {
+            new,
+            old_if_available,
+            ..
+        } => {
+            if let Some(member) = new
+                && let Some(old_member) = old_if_available
+            {
+                let mut counters = data.counters.lock().unwrap();
+
+                for counter in counters.iter_mut() {
+                    counter.member_updated(member, old_member);
+                }
+            }
         }
         _ => {}
     }
