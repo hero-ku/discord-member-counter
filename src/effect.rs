5037be484695c75b54cb0b6196468a379584a75b
Andrew Lynch <a_t_lynch@protonmail.ch>
2026-08-19 21:39:21 -0500

    Create struct for counter side-effects

---
+++ src/effect.rs
@@ -0,0 +1,28 @@
+use crate::Error;
+use poise::serenity_prelude::{self as serenity, ChannelId};
+
+#[derive(Clone)]
+pub enum CounterEffect {
+    ChannelLabel(ChannelId, String),
+}
+
+impl CounterEffect {
+    pub async fn handle_update(
+        &self,
+        ctx: std::sync::Arc<serenity::Http>,
+        count: u32,
+    ) -> Result<(), Error> {
+        match self {
+            CounterEffect::ChannelLabel(channel_id, prompt) => {
+                channel_id
+                    .edit(
+                        ctx,
+                        serenity::EditChannel::new().name(prompt.replace("{}", &count.to_string())),
+                    )
+                    .await
+            }
+        };
+
+        Ok(())
+    }
+}
